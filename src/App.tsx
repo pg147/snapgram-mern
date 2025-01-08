57a1ed0d570d640f8c798e8e6893c32c95dd9699
pg147 <xyzprathamesh147@gmail.com>
2025-01-09 00:11:01 +0530

    Routes set-up in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './globals.css'
+import SigninForm from './_auth/forms/SigninForm'
+import SignupForm from './_auth/forms/SignupForm'
+import { Homepage } from './_root/pages'
+
+// Layouts
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+    return (
+        <main className='flex h-screen'>
+            <Routes>
+                {/* public routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path='/sign-in' element={<SigninForm />} />
+                    <Route path='/sign-up' element={<SignupForm />} />
+                </Route>
+
+                {/* private routes */}
+                <Route element={<RootLayout />}>
+                    <Route index path='/' element={<Homepage />} />
+                </Route>
+            </Routes>
+        </main>
+    )
 }
