34329748a28df3373c8a914be67c4d92c02d3766
pg147 <xyzprathamesh147@gmail.com>
2025-01-09 01:06:18 +0530

    Auth Layout Render Logic updated

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,6 +1,25 @@
+import { Outlet, Navigate } from "react-router-dom";
 
 export default function AuthLayout() {
+  const isAuthenticated = false;
+
   return (
-    <div>AuthLayout</div>
+    <>
+      {isAuthenticated ? (
+        <Navigate to={"/"} />
+      ) : (
+        <>
+          <section className="flex flex-1 flex-col justify-center items-center py-10">
+            <Outlet />
+          </section>
+
+          <img 
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block w-1/2 h-screen object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
   )
 }
