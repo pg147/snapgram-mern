4d7b1b9e8d3ee9c88929580e68419f6dd9ac7145
pg147 <xyzprathamesh147@gmail.com>
2025-01-11 23:07:31 +0530

    Appwrite config updated

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -2,8 +2,13 @@ import { Client, Databases, Avatars, Account, Storage } from "appwrite";
 
 // Appwrite Config
 export const appwriteConfig = {
+    url: import.meta.env.VITE_APPWRITE_URL,
     projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    url: import.meta.env.VITE_APPWRITE_URL
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+    usersCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
+    postsCollectionId: import.meta.env.VITE_APPWRITE_POSTS_COLLECTION_ID,
+    savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID
 }
 
 // Client setup
