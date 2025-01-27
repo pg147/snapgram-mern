7c808f505ac68f6c670d96456d0787dd34b885af
pg147 <xyzprathamesh147@gmail.com>
2025-01-27 17:36:01 +0530

    Cookie fallback condition updated

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -77,7 +77,7 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     }
 
     useEffect(() => {
-        if (localStorage.getItem('cookieFallback') === '[]' || localStorage.getItem('cookieFallback') === null) {
+        if (localStorage.getItem('cookieFallback') === '[]') {
             navigate('/sign-in');
         }
 
