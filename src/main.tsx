b0e7cd80d4745d60f0d3825bb5f65e21cc7b9463
pg147 <xyzprathamesh147@gmail.com>
2025-01-27 17:36:30 +0530

    Wrapped app with providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import AuthProvider from "./context/AuthContext";
+import { QueryProvider } from "./lib/react-query/QueryProvider";
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </StrictMode>
+    <BrowserRouter>
+        <QueryProvider>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </QueryProvider>
+    </BrowserRouter>
 )
