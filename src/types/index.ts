9144f00fcd0ac85d7b081996a62e8ea1df9f8759
pg147 <xyzprathamesh147@gmail.com>
2025-01-27 18:49:55 +0530

    type index updated

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,24 @@
 export type INewUser = {
-    name: string,
-    email: string,
-    password: string,
-    username: string
-}
+  name: string;
+  email: string;
+  password: string;
+  username: string;
+};
 
 export type IUser = {
-    id: string;
-    name: string;
-    username: string;
-    email: string;
-    imageURL: string;
-    bio: string;
-  };
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  imageURL: string;
+  bio: string;
+};
+
+export type IContextType = {
+  user: IUser;
+  isLoading: boolean;
+  setUser: React.Dispatch<React.SetStateAction<IUser>>;
+  isAuthenticated: boolean;
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+  checkAuthUser: () => Promise<boolean>;
+};
